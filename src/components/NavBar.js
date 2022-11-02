4c718da19a42a57adda256ac19d321146ff32d80
wehrs-lucas <lwehrs@gmail.com>
2022-11-02 03:14:08 -0500

    Updated logo, favico, and app css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
 import '../css/NavBar.css'
-import logo from '../img/logo-sm.jpg'
+// import logo from '../img/logo-sm.jpg'
 
 export default function NavBar() {
     return(
         <nav className="nav-bar">
-            <img src={logo} alt='logo' className='nav-logo' />
+            <div className='nav-logo-text'>
+                <h3>TIC Loot</h3>
+            </div>
+            
             <ul className="nav-items">
-                <li><a href='http://localhost:3000/'>Home</a></li>
-                <li><a href='http://localhost:3000/'>Add Loot</a></li>
-                <li><a href='http://localhost:3000/'>Import</a></li>
+                <li><a href='https://tic-loot.com/'>Home</a></li>
+                <li><a href='https://tic-loot.com/'>Add Loot</a></li>
+                <li><a href='https://tic-loot.com/'>Import</a></li>
             </ul>
         </nav>
     )
